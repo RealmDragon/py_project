60063eb16c88eb5caff08bba14b6b06c61e8712d
RealmDragon <realmdragon2014@yandex.ru>
2024-05-20 15:16:08 +0300

    0.0 0.1 Добавлена Функция Сложения 0.2 Добавлена Функция Вычетания 0.3 Импортируеться Функция Сложения в main.py из funcs.py 0.4 Импортируеться Функция Вычетания в main.py из funcs.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from funcs import add
+from funcs import subtract
 
 # Пример использования функции сложения
 x = 5
 y = 10
 result = add(x, y)
 print(f"Сумма {x} и {y} равна {result}")
+
+# Пример использования функции вычитания
+x = 10
+y = 5
+result = subtract(x, y)
+print(f"Разность {x} и {y} равна {result}")
