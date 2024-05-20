084c50f56ecf1b1a8939c5ad59ed9dfc6d63c741
RealmDragon <realmdragon2014@yandex.ru>
2024-05-20 15:06:38 +0300

    0.0 0.1 Добавлена Функция Сложения 0.2 Добавлена Функция Вычетания

--- funcs.py
+++ funcs.py
@@ -9,3 +9,15 @@ def add(x, y):
     Сумма x и y.
   """
   return x + y
+
+def subtract(x, y):
+  """Вычитает два числа.
+
+  Args:
+    x: Первое число.
+    y: Второе число.
+
+  Returns:
+    Разность x и y.
+  """
+  return x - y
